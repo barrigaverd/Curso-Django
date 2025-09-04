a01aa4dc3dc26def847e83e1903c5aa96c2fd8a1
barrigaverd <sergiouine@gmail.com>
2025-09-04 10:13:20 -0300

    finalizado aula 37

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
+    cars = Car.objects.filter(model__contains = "M")
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': "Astra 2.0"} }
+        {'cars': cars }
     )
